e47de95ae6af632788d2071c6ca135bbf11a7b1b
Netanel Stern <netanel@ezsave.co.il>
2022-05-29 20:40:52 +0300

    basic text view ready to use

--- include/Views/TextView.h
+++ include/Views/TextView.h
@@ -1,9 +1,30 @@
 #pragma once
 #include "BaseView.h"
 
+#define DEFAULT_FRAME_MARGIN 10
+
 class TextView : public BaseView
 {
 public:
-	TextView();
+	TextView(std::string text, std::string fontName, unsigned int fontSize, int frameMargin = DEFAULT_FRAME_MARGIN);
 	void draw(sf::RenderWindow& window) override;
+
+	sf::Vector2f getPosition() const override;
+	void setPosition(sf::Vector2f position) override;
+	
+	void setFont(std::string fontName);
+	void setFontSize(int fontSize);
+	void setFrameMargin(float margin);
+	void setTextColor(sf::Color color);
+	void setBackgroundColor(sf::Color color);
+	
+	void setText(std::string text);
+	std::string getText() const;
+private:
+	void buildTextFrame();
+
+	sf::Text m_sfText;
+	sf::RectangleShape m_textFrame;
+
+	float m_frameMargin = DEFAULT_FRAME_MARGIN;
 };
